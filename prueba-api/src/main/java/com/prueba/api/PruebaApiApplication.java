20e20a4cfe023c816f41fa01d78f2f13774fa58c
ricardo-bc <andresbeltc@gmail.com>
2020-12-14 01:43:24 -0500

    new file:   prueba-api/.gitignore 	new file:   prueba-api/build.gradle 	new file:   prueba-api/gradle/wrapper/gradle-wrapper.jar 	new file:   prueba-api/gradle/wrapper/gradle-wrapper.properties 	new file:   prueba-api/gradlew 	new file:   prueba-api/gradlew.bat 	new file:   prueba-api/settings.gradle 	new file:   prueba-api/src/main/java/com/prueba/api/PruebaApiApplication.java 	new file:   prueba-api/src/main/resources/application.properties 	new file:   prueba-api/src/test/java/com/prueba/api/PruebaApiApplicationTests.java

---
+++ prueba-api/src/main/java/com/prueba/api/PruebaApiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApiApplication.class, args);
+	}
+
+}
